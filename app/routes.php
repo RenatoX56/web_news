84509246c1f6311bc61f347ea9051ad20f4791ce
Renato Roger <72948533+RenatoX56@users.noreply.github.com>
2022-04-27 14:00:53 -0400

    ruta admin

--- app/routes.php
+++ app/routes.php
@@ -2,4 +2,25 @@
 define('BASE_URL', 'http://localhost/web_news/');  
 define('PLUGINS', BASE_URL . 'plugins/');
 define('DIST', BASE_URL . 'dist/');
+
+//ruta admin
 define('ADMIN', BASE_URL . 'views/admin/');
+
+function getRoute($ruta = null){
+        $ruta = explode('/', $ruta);
+        $directorio = $ruta[0];
+        $archivo = $ruta[1];
+        $params = $ruta[2];
+    if ($params != '') {
+        $url = BASE_URL . 'views/' . $directorio . '/' . $archivo . '.php?' . $directorio . '=' . $params;
+    }elseif ($archivo != '') {
+        $url = BASE_URL . 'views/' . $directorio . '/' . $archivo . '.php';
+    }elseif ($directorio != '') {
+        $url = BASE_URL . 'views/' . $directorio . '/';
+    }else {
+        $url = BASE_URL;
+    }
+
+    $url = filter_var($url, FILTER_SANITIZE_URL);
+    return $url;
+}
